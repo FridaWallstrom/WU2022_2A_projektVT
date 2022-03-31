8055a007c6076342855c1ff6ca89769ffdb776f9
FridaWallstrom <93259332+FridaWallstrom@users.noreply.github.com>
2022-03-31 09:02:25 +0200

    v13

--- js/script.js
+++ js/script.js
@@ -11,12 +11,10 @@ burger.addEventListener("click", toggleMenu)
 var slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
