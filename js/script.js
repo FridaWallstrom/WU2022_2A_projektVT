d4be24fa7c0f0bab3aee06207bc2485b053a233b
FridaWallstrom <93259332+FridaWallstrom@users.noreply.github.com>
2022-04-03 22:17:25 +0200

    slutprodukt

--- js/script.js
+++ js/script.js
@@ -1,36 +1,44 @@
-const nav = document.getElementById("nav-links")
-const burger = document.getElementById("burger")
-
-function toggleMenu() {
-    nav.classList.toggle("nav-active")
-    burger.classList.toggle("crossed_line")
-}
-
-burger.addEventListener("click", toggleMenu) 
+const nav    = document.getElementById("nav-links");
+const burger = document.getElementById("burger");
+const prev   = document.getElementById("prev");
+const next   = document.getElementById("next");
+const slides = document.getElementsByClassName("mySlides");
 
 var slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+function showSlides() {
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+
+  if (slideIndex < 1) {
+    slideIndex = slides.length;
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
 } 
+
+function nextSlide() {
+  slideIndex++;
+  showSlides();
+}
+
+function prevSlide() {
+  slideIndex--;
+  showSlides();
+}
+
+function toggleMenu() {
+  nav.classList.toggle("nav-active");
+  burger.classList.toggle("crossed_line");
+}
+
+burger.addEventListener("click", toggleMenu);
+prev.addEventListener("click", prevSlide);
+next.addEventListener("click", nextSlide);
+
+showSlides();
